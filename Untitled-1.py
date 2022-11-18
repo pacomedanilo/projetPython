aada0d98322362eb870085d8e7197bc86ba54ef1
pacome <pacomedanilo503@gmail.com>
2022-11-18 15:25:34 +0100

    Morpion

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+
+class TicTacToe:
+
+    def __init__(self):
+        self.board = []
+
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+        print(create_board())
